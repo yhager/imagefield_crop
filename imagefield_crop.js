502ae72db5ed339aa333dda8f1e80c00fa3503d1
Yuval Hager <yhager@yhager.com>
2011-01-04 18:45:57 -0800

    preview shows existing file until user starts marking new crop. This overcomes the limitation in D7 that we cannot save the metadata with the file

--- imagefield_crop.js
+++ imagefield_crop.js
@@ -21,14 +21,17 @@ Drupal.behaviors.imagefield_crop = {
         // get the id attribute for multiple image support
         var self_id = self.attr('id');
         var id = self_id.substring(0, self_id.indexOf('-cropbox'));
-        // alert ("id is " + id);
-				//alert ("box width is " + settings.imagefield_crop[id].box.box_width);
-				//alert ("box height is " + settings.imagefield_crop[id].box.box_height);
         // get the name attribute for imagefield name
         var widget = self.parent().parent();
 
+          if ($(".edit-image-crop-changed", widget).val() == 1) {
+              $('.preview-existing', widget).css({display: 'none'});
+              $('.jcrop-preview', widget).css({display: 'block'});
+          }
+
         $(this).Jcrop({
           onChange: function(c) {
+            $('.preview-existing', widget).css({display: 'none'});
             var preview = $('.imagefield-crop-preview', widget);
             // skip newly added blank fields
             if (undefined == settings.imagefield_crop[id].preview) {
@@ -40,25 +43,30 @@ Drupal.behaviors.imagefield_crop = {
               width: Math.round(rx * settings.imagefield_crop[id].preview.orig_width) + 'px',
               height: Math.round(ry * settings.imagefield_crop[id].preview.orig_height) + 'px',
               marginLeft: '-' + Math.round(rx * c.x) + 'px',
-              marginTop: '-' + Math.round(ry * c.y) + 'px'
+              marginTop: '-' + Math.round(ry * c.y) + 'px',
+              display: 'block'
             });
           },
           onSelect: function(c) {
-              $(".edit-image-crop-x", widget).val(c.x);
-              $(".edit-image-crop-y", widget).val(c.y);
-              if (c.w) $(".edit-image-crop-width", widget).val(c.w);
-              if (c.h) $(".edit-image-crop-height", widget).val(c.h);
-              $(".edit-image-crop-changed", widget).val(1);
+            $('.preview-existing', widget).css({display: 'none'});
+            $(".edit-image-crop-x", widget).val(c.x);
+            $(".edit-image-crop-y", widget).val(c.y);
+            if (c.w) $(".edit-image-crop-width", widget).val(c.w);
+            if (c.h) $(".edit-image-crop-height", widget).val(c.h);
+            $(".edit-image-crop-changed", widget).val(1);
           },
           aspectRatio: settings.imagefield_crop[id].box.ratio,
           boxWidth: settings.imagefield_crop[id].box.box_width,
           boxHeight: settings.imagefield_crop[id].box.box_height,
+          /*
+           * Setting the select here calls onChange event, and we lose the original image visibility
           setSelect: [
             parseInt($(".edit-image-crop-x", widget).val()),
             parseInt($(".edit-image-crop-y", widget).val()),
             parseInt($(".edit-image-crop-width", widget).val()) + parseInt($(".edit-image-crop-x", widget).val()),
             parseInt($(".edit-image-crop-height", widget).val()) + parseInt($(".edit-image-crop-y", widget).val())
           ]
+*/
         });
       });
     };
